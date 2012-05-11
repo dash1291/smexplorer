18c050766b4baf09b13b61919a1fdcccda278069
Ashish Dubey <ashish.dubey91@gmail.com>
2012-05-11 19:10:10 +0530

    Add handle_delete to sync

--- sync/views.py
+++ sync/views.py
@@ -14,11 +14,9 @@ import storage_s3
 def handle_upload(request):
     if request.method == 'POST':
         remote_path = request.POST['path']
-        date_str = str(request.POST['last_modified'])
+        date_str = request.POST['last_modified']
         last_modified = datetime.strptime(date_str, '%Y-%m-%d')
-        f = request.FILES['file']
-        temp_path = fs.save_from_upload(remote_path, f)
-        file_info = fs.file_info(temp_path)
+        name = fs.file_name(remote_path)
         remote_parent = fs.parent_path(remote_path)
         try:
             remote_dir = Directory.objects.get(path=remote_parent)
@@ -26,13 +24,12 @@ def handle_upload(request):
             remote_dir = Directory(path=remote_parent)
             remote_dir.save()
         try:
-            tmp_file = File.objects.get(name=file_info['name'], path=remote_dir)
+            tmp_file = File.objects.get(name=name, path=remote_dir)
         except:
-            new_file = File(name=file_info['name'], path=remote_dir,
-                        size=file_info['size'],
+            new_file = File(name=name, path=remote_dir,
+                        size=request.POST['size'],
                         last_modified=last_modified)
             new_file.save()
-        storage_s3.upload_file(temp_path, remote_path)
         response = render(request, 'response.xml',
                     {'status': '1', 'message': 'File uploaded successfully.'},
                     content_type='text/xml')
@@ -40,4 +37,29 @@ def handle_upload(request):
         response = render(request, 'response.xml',
                    {'status': '0', 'message': 'Use POST for file uploads.'},
                    content_type='text/xml')
+    return response
+
+@csrf_except
+def handle_delete():
+    if request.method == 'POST': 
+        remote_path = request.POST['path']
+        date_str = request.POST['last_modified']
+        last_modified = datetime.strptime(date_str, '%Y-%m-%d')
+        name = fs.file_name(remote_path)
+        remote_parent = fs.parent_path(remote_path)
+        try:
+            remote_dir = Directory.objects.get(path=remote_parent)
+            tmp_file = File.objects.get(name=name, path=remote_dir)
+            new_file.save()
+            response = render(request, 'response.xml',
+                        {'status': '1', 'message': 'File delete succesfully.'},
+                        content_type='text/xml')
+        except:
+            response = render(request, 'response.xml',
+                        {'status': '0', 'message': 'Path does not exist.'},
+                        content_type='text/xml')
+    else:
+        response = render(request, 'response.xml',
+                    {'status': '0', 'message': 'Use POST for deleting a file.'},
+                    content_type='text/xml')
     return response
