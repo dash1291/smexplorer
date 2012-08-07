1665fd4713586f150ed74f04ad2c178c007b91ee
namitsingal <namitsingal@gmail.com>
2012-08-07 22:58:04 +0530

    added __unicode__()

--- explorer/models.py
+++ explorer/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Directory(models.Model):
     path = models.TextField(primary_key=True)
+    def __unicode__(self):
+        return self.path
+
 
 class File(models.Model):
     id = models.AutoField(primary_key=True)
