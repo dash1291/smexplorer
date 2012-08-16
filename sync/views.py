16bd154ce0b791a038c9fd246fab406513faed08
Ashish Dubey <ashish.dubey91@gmail.com>
2012-08-16 21:49:21 +0530

    Add bulk_upload & bulk_delete

--- sync/views.py
+++ sync/views.py
@@ -50,7 +50,7 @@ def handle_delete(request):
         except:
             response = render(request, 'response.xml',
                        {'status': '0', 'message': 'File not found.'},
-                       content_type='text/xml')
+                       co ntent_type='text/xml')
             return response
         tmp_file.delete()
         response = render(request, 'response.xml',
@@ -63,3 +63,67 @@ def handle_delete(request):
                    content_type='text/xml')
     return response
 
+@csrf_exempt
+def bulk_upload(request):
+	if request.method == 'POST':
+        report = []
+        file_data = request.POST['files']
+        files = json.loads(file_data)
+
+        for entry in files:
+            remote_path = entry['path']
+            date_str = entry['last_modified']
+            size = entry['size']
+            last_modified = datetime.strptime(date_str, '%Y-%m-%d')
+            name = fs.file_name(remote_path)
+            remote_parent = fs.parent_path(remote_path)
+
+            try:
+                remote_dir = Directory.objects.get(path=remote_parent)
+            except:
+                remote_dir = Directory(path=remote_parent)
+                remote_dir.save()
+
+            try:
+                tmp_file = File.objects.get(name=name, path=remote_dir)
+            except:
+                new_file = File(name=name, path=remote_dir,
+                        size=size, last_modified=last_modified)
+                new_file.save()
+
+            report.append({'status': '1','name': name, 
+                'message': 'File uploaded successfully.'})
+
+    else:
+        report.append({'status': '0', 'message': 'Use POST for file uploads.'})
+ 
+    response = HttpResponse(json.dumps(report))
+    return response
+
+@csrf_exempt
+def bulk_delete(request):
+    if request.method == 'POST':
+        report = []
+        file_data= request.POST['files']
+        files = json.load(file_data)
+
+        for entry in files:            
+            remote_path = request.POST['path']
+            name = fs.file_name(remote_path)
+            remote_parent = fs.parent_path(remote_path)
+            try:
+                remote_dir = Directory.objects.get(path=remote_parent)
+                tmp_file = File.objects.get(name=name, path=remote_dir)
+                tmp_file.delete()
+                report.append({'status': '1', 'name': name,
+                    'message': 'File delete succesfully.'})
+            except:
+                report.append({'status': '0', 'name': name,
+                    'message': 'Path does not exist.'})
+    else:
+        report.append({'status': '0',
+            'message': 'Use POST for deleting a file.'})
+
+    response = render(request, 'response.xml', {'report': report},
+            content_type='text/xml')
+    return response
