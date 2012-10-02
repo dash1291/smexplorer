ef8cc436367f520223a4fff28917f2539266d6e9
Ashish Dubey <ashish.dubey91@gmail.com>
2012-10-02 22:48:36 +0530

    add /api

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+
 import explorer
+from api.urls import v1_api
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,4 +19,5 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('explorer.urls')),
     url(r'^sync/', include('sync.urls')),
+    url(r'^api/', include(v1_api.urls)),
 )
