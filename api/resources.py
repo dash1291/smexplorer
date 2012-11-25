4a0b542a84e187bc13b2650e92d57d6d9bd70de5
Ashish Dubey <ashish.dubey91@gmail.com>
2012-11-25 16:27:10 +0530

    Prettyify API response and add dummy fixtures

--- api/resources.py
+++ api/resources.py
@@ -1,14 +1,40 @@
+import os.path
+
+from tastypie import fields
 from tastypie.resources import ModelResource
 
 from explorer.models import File, Directory
-
-class FileResource(ModelResource):
-    class Meta:
-        queryset = File.objects.all()
-        allowed_methods = ['get']
-
 
 class DirectoryResource(ModelResource):
     class Meta:
         queryset = Directory.objects.all()
         allowed_methods = ['get']
+
+    def dehydrate(self, bundle):
+        d = bundle.obj
+        files_query = d.file_set.all()
+        dirs_query = Directory.objects.filter(path__regex='^' + os.path.join(d.path + '[^/]+$'))
+
+        files = []
+        dirs = []
+        for f in files_query:
+            files.append({'id': f.id, 
+                          'path': os.path.join(f.path.path[1:], f.name)})
+
+        for d in dirs_query:
+            dirs.append({'id': d.id, 'path': d.path[1:]})
+
+        bundle.data['files'] = files
+        bundle.data['subdirectories'] = dirs
+        bundle.data['path'] = bundle.data['path'][1:]
+        return bundle
+
+class FileResource(ModelResource):
+    path = fields.ForeignKey(DirectoryResource, 'path')
+
+    class Meta:
+        queryset = File.objects.all()
+        allowed_methods = ['get']
+
+    def dehydrate(self, bundle):
+        return bundle
