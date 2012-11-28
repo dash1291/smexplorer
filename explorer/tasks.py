f431431f0b009db80be51d1acc9e419730740b52
Ashish Dubey <ashish.dubey91@gmail.com>
2012-11-28 17:41:49 +0530

    Fix delete_archive task

--- explorer/tasks.py
+++ explorer/tasks.py
@@ -1,8 +1,9 @@
 import os
+import os.path
 
 from settings import APP_STORAGE_PATH
 from celery import task
 
 @task()
 def delete_archive(zip_path):
-    os.remove(APP_STORAGE_PATH + zip_path)
+    os.remove(os.path.join(APP_STORAGE_PATH, zip_path))
